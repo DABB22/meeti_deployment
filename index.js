36b44ff57b9ce5d38a88320ebfca892af9f9bd67
Diego <di34nd@gmail.com>
2023-12-06 10:50:32 -0500

    habilitamos variable env.HOST

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use('/', router());
 // });
 
 //leer el host y el puerto
-// const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log('El servidor esta funcionando en el puerto', port);
